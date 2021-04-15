dfe0840513348f168ce71fbe106611e32ac751b8
arvindp44 <ap427@duke.edu>
2021-04-15 17:30:34 -0400

    Wrote out initial functions

--- final.py
+++ final.py
@@ -3,5 +3,24 @@ Final Project
 Name:
 Date:
 NetID(s):
+"""
 
-"""""""""""""""""""""""""""""""
+def dx(x,y):
+    dx = x(e1-a1*x - c1*y)
+    return dx
+
+def dy(x,y):
+    dy = y(e2-a2*y-c2*x)
+
+
+
+
+##
+##def fx(x,y):
+##    fx =(2-x-y)*x
+##    return fx
+##
+##def fy(x,y):
+##    fy =(3-3*x-y)*y
+##    return fy
+
