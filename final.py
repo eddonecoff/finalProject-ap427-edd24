9e7334724ae8bd9ccb11719f22181f99d1aee3a0
eddonecoff <ethan.donecoff@duke.edu>
2021-04-16 16:38:18 -0400

    add second population plot

--- final.py
+++ final.py
@@ -36,9 +36,6 @@ def fy(x,y):
    fy = -y + x*y
    return fy
 
-x = []
-y = []
-
 """
 
 Modeling population changes
@@ -46,12 +43,15 @@ Modeling population changes
 """
 
 def pop(x0, y0, dt, time):
+	x = []
+	y = []
+
 	# Initial values
 	x.append(x0)
 	y.append(y0)
 
 	# Calculate populations at each timestep
-	for i in range(time):
+	for i in range(time - 1):
 		x.append(x[i] + (fx(x[i],y[i])) * dt)
 		y.append(y[i] + (fy(x[i],y[i])) * dt)
 
@@ -63,26 +63,32 @@ Plotting population changes
 
 """
 
-x, y = pop(5, 3, 0.01, 1000)
-a, b = pop(4, 3, 0.01, 1000)
+x1, y1 = pop(5, 3, 0.01, 1000)
+x2, y2 = pop(0.5, 0.5, 0.01, 1000)
+timelist = [0.01*i for i in range(1000)]
 
 fig = plt.figure(figsize=(15,5))
 fig.subplots_adjust(wspace = 0.5, hspace = 0.3)
 ax1 = fig.add_subplot(1,2,1)
 ax2 = fig.add_subplot(1,2,2)
 
-ax1.plot(x, 'r-', label='predator')
-ax1.plot(y, 'b-', label='prey')
+ax1.plot(timelist, x1, 'r-', label='predator')
+ax1.plot(timelist, y1, 'b-', label='prey')
+ax1.plot(timelist, x2, label = "predator 2")
+ax1.plot(timelist, y2, label = "prey 2")
 ax1.set_title("Dynamics in time")
 ax1.set_xlabel("time")
 ax1.grid()
 ax1.legend(loc='best')
 
-ax2.plot(x, y, color="blue")
+ax2.plot(x1, y1, color="blue")
+ax2.plot(x2, y2)
 ax2.set_xlabel("x")
 ax2.set_ylabel("y")  
 ax2.set_title("Phase space")
 ax2.grid()
+
+plt.show()
 
 
 """
