716547c816d1f32fb8c26a02c723803958abf7e0
arvindp44 <ap427@duke.edu>
2021-04-16 17:55:54 -0400

    Added Rk4 function

--- final.py
+++ final.py
@@ -7,6 +7,7 @@ NetID(s):
 
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 """
 
@@ -42,112 +43,140 @@ Modeling population changes
 
 """
 
-def pop(x0, y0, dt, time):
-	x = []
-	y = []
-
-	# Initial values
-	x.append(x0)
-	y.append(y0)
-
-	# Calculate populations at each timestep
-	for i in range(time - 1):
-		x.append(x[i] + (fx(x[i],y[i])) * dt)
-		y.append(y[i] + (fy(x[i],y[i])) * dt)
-
-	return(x,y)
+##def pop(x0, y0, dt, time):
+##	x = []
+##	y = []
+##
+##	# Initial values
+##	x.append(x0)
+##	y.append(y0)
+##
+##	# Calculate populations at each timestep
+##	for i in range(time - 1):
+##		x.append(x[i] + (fx(x[i],y[i])) * dt)
+##		y.append(y[i] + (fy(x[i],y[i])) * dt)
+##
+##	return(x,y)
 
 """
 
 List of Initial Populations
 
-"""
-def init_pops(numPoints):
-
-	init_pops= []
-	# step = 5/numPoints
-
-	# for i in range(numPoints):
-		
-	# 	x = i*step
-	# 	y = 5 - i*step
-	# 	init_pops.append((x,y))
-
-	# return(init_pops)
-
-	for i in range(numPoints):
-		x = random.random()*5
-		y = random.random()*5
-		init_pops.append((x,y))
-
-	return(init_pops)
+##"""
+##def init_pops(numPoints):
+##
+##	init_pops= []
+##	# step = 5/numPoints
+##
+##	# for i in range(numPoints):
+##		
+##	# 	x = i*step
+##	# 	y = 5 - i*step
+##	# 	init_pops.append((x,y))
+##
+##	# return(init_pops)
+##
+##	for i in range(numPoints):
+##		x = random.random()*5
+##		y = random.random()*5
+##		init_pops.append((x,y))
+##
+##	return(init_pops)
 
 """
 
 Plotting population changes
 
 """
-
-x1, y1 = pop(0, 4, 0.01, 1000)
-x2, y2 = pop(4, 0, 0.01, 1000)
-timelist = [0.01*i for i in range(1000)]
-
-fig = plt.figure(figsize=(15,5))
-fig.subplots_adjust(wspace = 0.5, hspace = 0.3)
-ax1 = fig.add_subplot(1,2,1)
-ax2 = fig.add_subplot(1,2,2)
-
-ax1.plot(timelist, x1, 'r-', label='predator')
-ax1.plot(timelist, y1, 'b-', label='prey')
-ax1.plot(timelist, x2, label = "predator 2")
-ax1.plot(timelist, y2, label = "prey 2")
-
-init_pops = init_pops(20)
-
-for i in range(len(init_pops)):
-	a, b = init_pops[i]
-	x, y = pop(a, b, 0.01, 1000)
-	ax1.plot(timelist, x)
-	ax1.plot(timelist, y)
-
-	ax2.plot(x, y)
-
-ax1.set_title("Dynamics in time")
-ax1.set_xlabel("time")
-ax1.grid()
-ax1.legend(loc='best')
-
-ax2.plot(x1, y1, color="blue")
-ax2.plot(x2, y2)
-ax2.set_xlabel("x")
-ax2.set_ylabel("y")  
-ax2.set_title("Phase space")
-ax2.grid()
-
-plt.show()
-
-
-"""
-Determine Steady States
-"""
-fp = []
-
-def find_fixed_points(r):
-    for x in range(r):
-        for y in range(r):
-            if ((fx(x,y) == 0) and (fy(x,y) == 0)):
-                fp.append((x,y))
-                print('The system has a fixed point in %s,%s' % (x,y))
-    return fp
-
-print(find_fixed_points(200))
-
-
+##
+##x1, y1 = pop(0, 4, 0.01, 1000)
+##x2, y2 = pop(4, 0, 0.01, 1000)
+##timelist = [0.01*i for i in range(1000)]
+##
+##fig = plt.figure(figsize=(15,5))
+##fig.subplots_adjust(wspace = 0.5, hspace = 0.3)
+##ax1 = fig.add_subplot(1,2,1)
+##ax2 = fig.add_subplot(1,2,2)
+##
+##ax1.plot(timelist, x1, 'r-', label='predator')
+##ax1.plot(timelist, y1, 'b-', label='prey')
+##ax1.plot(timelist, x2, label = "predator 2")
+##ax1.plot(timelist, y2, label = "prey 2")
+##
+##init_pops = init_pops(20)
+##
+##for i in range(len(init_pops)):
+##	a, b = init_pops[i]
+##	x, y = pop(a, b, 0.01, 1000)
+##	ax1.plot(timelist, x)
+##	ax1.plot(timelist, y)
+##
+##	ax2.plot(x, y)
+##
+##ax1.set_title("Dynamics in time")
+##ax1.set_xlabel("time")
+##ax1.grid()
+##ax1.legend(loc='best')
+##
+##ax2.plot(x1, y1, color="blue")
+##ax2.plot(x2, y2)
+##ax2.set_xlabel("x")
+##ax2.set_ylabel("y")  
+##ax2.set_title("Phase space")
+##ax2.grid()
+##
+##plt.show()
+##
+##
+##"""
+##Determine Steady States
+##"""
+##fp = []
+##
+##def find_fixed_points(r):
+##    for x in range(r):
+##        for y in range(r):
+##            if ((fx(x,y) == 0) and (fy(x,y) == 0)):
+##                fp.append((x,y))
+##                print('The system has a fixed point in %s,%s' % (x,y))
+##    return fp
+##
+##print(find_fixed_points(200))
+##
+##
 """
 
 RK4
 
 """
+
+
+
+
+def fxy(x,y):
+   return np.array([[fx(x,y)], [fy(x,y)]])
+
+def rk4(N,a,b,dt):
+   x0 = np.array([[a],[b]])
+   xn = np.zeros((2,N+1))
+   xn[:,0,None] = x0
+   t = [dt*n for n in range(N)]
+   
+   for n in range(N):
+      tn = t[n]
+      a = xn[0][n]
+      b = xn[1][n]
+      k1 = dt*fxy(a,b)
+      k2 = dt*fxy(a+(k1[0][0])/2, b+(k1[1][0])/2)
+      k3 = dt*fxy(a+(k2[0][0])/2, b+(k2[1][0])/2)
+      k4 = dt*fxy(a+(k3[0][0]), b+(k3[1][0]))
+
+      xn[:,n+1,None] = xn[:,n,None] + 1/6*k1 + 1/3*k2 + 1/3*k3 + 1/6*k4
+         
+   return xn
+print(rk4(1000,5,3,0.1))
+
+
 # plt.figure()
 # 	fig, ax = plt.subplots()
 # 	ax.plot(N, errorRK4, label = "Error vs. N")
