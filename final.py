c3fe27fb057e81e4c29598c41f7c8d8ba80d5175
eddonecoff <ethan.donecoff@duke.edu>
2021-04-19 14:36:41 -0400

    Comment and clean-up

--- final.py
+++ final.py
@@ -11,24 +11,10 @@ import numpy as np
 
 """
 
-Actual Function
+This competitive species system of ODEs shows a spiral 
+sink at (1,1)
 
 """
-
-##def fx(x,y):
-##   fx =(2-x-y)*x
-##   return fx
-##
-##def fy(x,y):
-##   fy =(3-3*x-y)*y
-##   return fy
-
-"""
-
-Example Function
-
-"""
-
 def fx(x,y):
    fx = 2*x - x**2 - x*y
    return fx
@@ -39,11 +25,10 @@ def fy(x,y):
 
 """
 
-Another Function
+This competitive species system of ODEs shows a nodal
+source at (0,0)
 
 """
-
-# This system shows a source at (0,0)
 def fx1(x,y):
 	fx1 = (1-x-y)*x
 	return fx1
@@ -52,7 +37,12 @@ def fy1(x,y):
 	fy1 = (0.75-y-0.5*x)*y
 	return fy1
 
-# This system shows a saddle at (0,4) 
+"""
+
+This competitive species system of ODEs shows a saddle 
+at (0,1) and a nodal sink at (3,0).
+
+"""
 def fx2(x,y):
 	fx1 = (6-2*x-3*y)*x
 	return fx1
@@ -61,6 +51,12 @@ def fy2(x,y):
 	fy1 = (1-x-y)*y
 	return fy1
 
+"""
+
+This competitive species system of ODes shows a spiral 
+source at (0,0).
+
+"""
 def fx3(x,y):
 	fx1 = 0.2*x+y
 	return fx1
@@ -71,10 +67,10 @@ def fy3(x,y):
 
 """
 
-Modeling population changes
+This performs forward Euler to roughly approximate x(t) and y(t)
 
 """
-
+"""
 def pop(x0, y0, dt, time):
 	x = []
 	y = []
@@ -92,11 +88,20 @@ def pop(x0, y0, dt, time):
 
 # p1, p2 = pop(-0.5, -0.5, 0.1, 10)
 # print(len(p1))
-
 """
 
-List of Initial Populations
+"""
+init_pops
 
+This function generates a list of random initial populations.
+
+INPUTS:
+numPoints: The number of initial populations generated
+xRange: Range of initial x populations
+yRange: Range of initial y populations
+
+OUTPUTS:
+init_pops: List of initial populations
 """
 def init_pops(numPoints, xRange, yRange):
 
@@ -111,75 +116,43 @@ def init_pops(numPoints, xRange, yRange):
 
 """
 
-Plotting population changes
+Define two sets of initial populations to be used below for
+modeling the four different systems.
 
 """
 
-# x1, y1 = pop(0, 4, 0.01, 1000)
-# x2, y2 = pop(4, 0, 0.01, 1000)
-# timelist = [0.01*i for i in range(1000)]
-
-# fig = plt.figure(figsize=(15,5))
-# fig.subplots_adjust(wspace = 0.5, hspace = 0.3)
-# ax1 = fig.add_subplot(1,2,1)
-# ax2 = fig.add_subplot(1,2,2)
-
-# ax1.plot(timelist, x1, 'r-', label='predator')
-# ax1.plot(timelist, y1, 'b-', label='prey')
-# ax1.plot(timelist, x2, label = "predator 2")
-# ax1.plot(timelist, y2, label = "prey 2")
-
+# Initial populations in 5x5 square in first quadrant
 init_pops1 = init_pops(100,5,5)
+
+# Initial populations in 0.5x0.5 square in first quadrant
 init_pops2 = init_pops(100,0.5,0.5)
 
-# for i in range(len(init_pops)):
-# 	a, b = init_pops[i]
-# 	x, y = pop(a, b, 0.01, 1000)
-# 	ax1.plot(timelist, x)
-# 	ax1.plot(timelist, y)
-
-# 	ax2.plot(x, y)
-
-# ax1.set_title("Dynamics in time")
-# ax1.set_xlabel("time")
-# ax1.grid()
-# ax1.legend(loc='best')
-
-# ax2.plot(x1, y1, color="blue")
-# ax2.plot(x2, y2)
-# ax2.set_xlabel("x")
-# ax2.set_ylabel("y")  
-# ax2.set_title("Phase space")
-# ax2.grid()
-
-# plt.show()
-
 
 """
+find_eqpts
 
-Determine Steady States
+This function finds the equilibrium points (location where fx = fy = 0)
+for a system of ODEs
+
+INPUTS:
+r: range of (x,y) values that will be checked
+
+OUTPUTS:
+fp: list of equilibrium points
 
 """
 fp = []
 
-def find_fixed_points(r):
+def find_eqpts(r):
    for x in range(r):
        for y in range(r):
            if ((fx1(x,y) == 0) and (fy1(x,y) == 0)):
-               fp.append((x,y))
-               print('The system has a fixed point in %s,%s' % (x,y))
-   return fp
-
-print(find_fixed_points(1000))
+               ep.append((x,y))
+               print('The system has an equilibrium point at %s,%s' % (x,y))
+   return ep
 
 
-"""
-
-RK4
-
-"""
-
-# vector function for growth rate of both populations
+# Vector function (np array) for growth rate of both populations
 def fxy(x,y):
   	return np.array([[fx(x,y)], [fy(x,y)]])
 
@@ -192,7 +165,22 @@ def fxy2(x,y):
 def fxy3(x,y):
 	return np.array([[fx3(x,y)], [fy3(x,y)]])
 
-# RK4 simulation of competitive species system
+"""
+RK4
+
+This function uses RK4 to approximate x(t) and y(t).
+
+INPUTS:
+N: Number of subintervals
+a: Initial x population x(0)
+b: initial y population y(0)
+dt: timestep
+
+OUTPUTS:
+t: List of times
+xn: List of (x,y) population at each time
+"""
+
 def rk4(N,a,b,dt):
    x0 = np.array([[a],[b]])
    xn = np.zeros((2,N+1))
@@ -270,127 +258,133 @@ def rk4_3(N,a,b,dt):
          
    return t, xn
 
-# Example function
 
-# Phase-Plane Portrait
-plt.figure()
-fig, ax = plt.subplots()
-plt.title("Phase-Plane Portrait Using RK4")
-plt.xlabel("Prey Population")
-plt.ylabel("Predator Population")
+"""
+main function
+"""
+if __name__ == '__main__':
 
-# Draw portrait for 20 random initial pops using RK4
-for i in range(len(init_pops1)):
-	a, b = init_pops1[i]
-	t, xn = rk4(1000, a, b, 0.01)
-	ax.plot(xn[0], xn[1])
+	# fx, fy
 
-plt.savefig("rk4ppp.png", bbox_inches = "tight")
-plt.close("all")
+	# Phase-Plane Portrait
+	plt.figure()
+	fig, ax = plt.subplots()
+	plt.title("Phase-Plane Portrait Using RK4")
+	plt.xlabel("Prey Population")
+	plt.ylabel("Predator Population")
 
-# Example population plot with initial (5,3)
-t, xn = rk4(1000, 5, 3, 0.01)
-plt.figure()
-fig, ax = plt.subplots()
-ax.plot(t, xn[0], label = "Prey Population")
-ax.plot(t, xn[1], label = "Predator Population")
-ax.legend(loc = "upper right")
-plt.title("Species Population over Time")
-plt.xlabel("Time")
-plt.ylabel("Population")
-plt.savefig("rk4pop.png", bbox_inches = "tight")
-plt.close("all")
+	# Draw portrait for 20 random initial pops using RK4
+	for i in range(len(init_pops1)):
+		a, b = init_pops1[i]
+		t, xn = rk4(1000, a, b, 0.01)
+		ax.plot(xn[0], xn[1])
+
+	plt.savefig("rk4_0ppp.png", bbox_inches = "tight")
+	plt.close("all")
+
+	# Example population plot with initial (5,3)
+	t, xn = rk4(1000, 5, 3, 0.01)
+	plt.figure()
+	fig, ax = plt.subplots()
+	ax.plot(t, xn[0], label = "Prey Population")
+	ax.plot(t, xn[1], label = "Predator Population")
+	ax.legend(loc = "upper right")
+	plt.title("Species Population over Time")
+	plt.xlabel("Time")
+	plt.ylabel("Population")
+	plt.savefig("rk4_0pop.png", bbox_inches = "tight")
+	plt.close("all")
 
 
-# fxy1: nodal source near (0,0)
+	# fx1, fy1
 
-# Phase-Plane Portrait
-plt.figure()
-fig, ax = plt.subplots()
-plt.title("Phase-Plane Portrait Using RK4")
-plt.xlabel("Prey Population")
-plt.ylabel("Predator Population")
+	# Phase-Plane Portrait
+	plt.figure()
+	fig, ax = plt.subplots()
+	plt.title("Phase-Plane Portrait Using RK4")
+	plt.xlabel("Prey Population")
+	plt.ylabel("Predator Population")
 
-# Draw portrait for 20 random initial pops using RK4
-for i in range(len(init_pops2)):
-	a1, b1 = init_pops2[i]
-	t1, xn1 = rk4_1(1000, a1, b1, 0.01)
-	ax.plot(xn1[0], xn1[1])
+	# Draw portrait for 20 random initial pops using RK4
+	for i in range(len(init_pops2)):
+		a1, b1 = init_pops2[i]
+		t1, xn1 = rk4_1(1000, a1, b1, 0.01)
+		ax.plot(xn1[0], xn1[1])
 
-plt.savefig("rk4_1ppp.png", bbox_inches = "tight")
-plt.close("all")
+	plt.savefig("rk4_1ppp.png", bbox_inches = "tight")
+	plt.close("all")
 
-# Example population plot with initial (5,3)
-t1, xn1 = rk4_1(1000, 5, 3, 0.01)
-plt.figure()
-fig, ax = plt.subplots()
-ax.plot(t1, xn1[0], label = "Prey Population")
-ax.plot(t1, xn1[1], label = "Predator Population")
-ax.legend(loc = "upper right")
-plt.title("Species Population over Time")
-plt.xlabel("Time")
-plt.ylabel("Population")
-plt.savefig("rk4_1pop.png", bbox_inches = "tight")
-plt.close("all")
+	# Example population plot with initial (5,3)
+	t1, xn1 = rk4_1(1000, 5, 3, 0.01)
+	plt.figure()
+	fig, ax = plt.subplots()
+	ax.plot(t1, xn1[0], label = "Prey Population")
+	ax.plot(t1, xn1[1], label = "Predator Population")
+	ax.legend(loc = "upper right")
+	plt.title("Species Population over Time")
+	plt.xlabel("Time")
+	plt.ylabel("Population")
+	plt.savefig("rk4_1pop.png", bbox_inches = "tight")
+	plt.close("all")
 
-# fxy2: 
+	# fx2, fy2 
 
-# Phase-Plane Portrait
-plt.figure()
-fig, ax = plt.subplots()
-plt.title("Phase-Plane Portrait Using RK4")
-plt.xlabel("Prey Population")
-plt.ylabel("Predator Population")
+	# Phase-Plane Portrait
+	plt.figure()
+	fig, ax = plt.subplots()
+	plt.title("Phase-Plane Portrait Using RK4")
+	plt.xlabel("Prey Population")
+	plt.ylabel("Predator Population")
 
-# Draw portrait for 20 random initial pops using RK4
-for i in range(len(init_pops1)):
-	a2, b2 = init_pops1[i]
-	t2, xn2 = rk4_2(1000, a2, b2, 0.01)
-	ax.plot(xn2[0], xn2[1])
+	# Draw portrait for 20 random initial pops using RK4
+	for i in range(len(init_pops1)):
+		a2, b2 = init_pops1[i]
+		t2, xn2 = rk4_2(1000, a2, b2, 0.01)
+		ax.plot(xn2[0], xn2[1])
 
-plt.savefig("rk4_2ppp.png", bbox_inches = "tight")
-plt.close("all")
+	plt.savefig("rk4_2ppp.png", bbox_inches = "tight")
+	plt.close("all")
 
-# Example population plot with initial (5,3)
-t2, xn2 = rk4_2(1000, 5, 3, 0.01)
-plt.figure()
-fig, ax = plt.subplots()
-ax.plot(t2, xn2[0], label = "Prey Population")
-ax.plot(t2, xn2[1], label = "Predator Population")
-ax.legend(loc = "upper right")
-plt.title("Species Population over Time")
-plt.xlabel("Time")
-plt.ylabel("Population")
-plt.savefig("rk4_2pop.png", bbox_inches = "tight")
-plt.close("all")
+	# Example population plot with initial (5,3)
+	t2, xn2 = rk4_2(1000, 5, 3, 0.01)
+	plt.figure()
+	fig, ax = plt.subplots()
+	ax.plot(t2, xn2[0], label = "Prey Population")
+	ax.plot(t2, xn2[1], label = "Predator Population")
+	ax.legend(loc = "upper right")
+	plt.title("Species Population over Time")
+	plt.xlabel("Time")
+	plt.ylabel("Population")
+	plt.savefig("rk4_2pop.png", bbox_inches = "tight")
+	plt.close("all")
 
-# fxy3: experimental
+	# fx3, fy3
 
-# Phase-Plane Portrait
-plt.figure()
-fig, ax = plt.subplots()
-plt.title("Phase-Plane Portrait Using RK4")
-plt.xlabel("Prey Population")
-plt.ylabel("Predator Population")
+	# Phase-Plane Portrait
+	plt.figure()
+	fig, ax = plt.subplots()
+	plt.title("Phase-Plane Portrait Using RK4")
+	plt.xlabel("Prey Population")
+	plt.ylabel("Predator Population")
 
-# Draw portrait for 20 random initial pops using RK4
-for i in range(len(init_pops1)):
-	a3, b3 = init_pops1[i]
-	t3, xn3 = rk4_3(1000, a3, b3, 0.01)
-	ax.plot(xn3[0], xn3[1])
+	# Draw portrait for 20 random initial pops using RK4
+	for i in range(len(init_pops1)):
+		a3, b3 = init_pops1[i]
+		t3, xn3 = rk4_3(1000, a3, b3, 0.01)
+		ax.plot(xn3[0], xn3[1])
 
-plt.savefig("rk4_3ppp.png", bbox_inches = "tight")
-plt.close("all")
+	plt.savefig("rk4_3ppp.png", bbox_inches = "tight")
+	plt.close("all")
 
-# Example population plot with initial (5,3)
-t3, xn3 = rk4_3(1500, 5, 3, 0.01)
-plt.figure()
-fig, ax = plt.subplots()
-ax.plot(t3, xn3[0], label = "Predator Population")
-ax.plot(t3, xn3[1], label = "Prey Population")
-ax.legend(loc = "upper right")
-plt.title("Species Population over Time")
-plt.xlabel("Time")
-plt.ylabel("Population")
-plt.savefig("rk4_3pop.png", bbox_inches = "tight")
-plt.close("all")
+	# Example population plot with initial (5,3)
+	t3, xn3 = rk4_3(1500, 5, 3, 0.01)
+	plt.figure()
+	fig, ax = plt.subplots()
+	ax.plot(t3, xn3[0], label = "Predator Population")
+	ax.plot(t3, xn3[1], label = "Prey Population")
+	ax.legend(loc = "upper right")
+	plt.title("Species Population over Time")
+	plt.xlabel("Time")
+	plt.ylabel("Population")
+	plt.savefig("rk4_3pop.png", bbox_inches = "tight")
+	plt.close("all")
